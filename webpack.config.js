34ec84c455c160bc33743d37acfe05c8ce8ebf1e
Dylan Connor <DylanJConnor4@gmail.com>
2020-08-06 23:23:37 -0400

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: __dirname + '/docs/',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test:/\.(s*)css$/,
+        use: [
+
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader"
+
+        ]
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+    }
+    ]
+  },
+  node: {
+    fs: 'empty'
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./index.html",
+      filename: "./index.html"
+    })
+  ]
+};
