ce8fe5160e3e7a6c7a4e6148c9256603ddaaf6f4
Dylan Connor <DylanJConnor4@gmail.com>
2020-08-09 15:09:51 -0400

    run build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
   output: {
     path: __dirname + '/docs/',
-    publicPath: '/'
+    publicPath: '/react-wp/'
   },
   module: {
     rules: [
